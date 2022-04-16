bbdb83b9d6670add0a87dca1ee24c2ff24bf9139
Bob <rwillford0719@gmail.com>
2022-04-15 19:34:19 -0500

    everything wokring minus repeated search history name, date in 5 day not correct, and search history isn't working when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,116 +1,157 @@
 $(document).ready(function(){
-
     const apiKey = 'dc29ad80b0ec9912243321a6824d9baa';
-
-$("#searchBtn").on("click", function(){
-    let citySearch = $("#searchInput").val()
-   getCurrentWeather(citySearch)
-   //Clear value after search so you can't spam the same city
-   $("#searchInput").val("");
-   //Clearing Out so it doesn't stack
-   $(".currentWeather").empty();
-   $(".fiveDay").empty();
-
-})
-
-function getCurrentWeather(citySearch){
-    //API for Current Weather
-    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&units=imperial&appid=${apiKey}`
-
-    fetch(currentWeatherUrl)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log("currentDay", data)
-        //Setting the data needed
-        let cityName = data.name;
-        let temp = data.main.temp;
-        let humidity = data.main.humidity;
-        let wind = data.wind.speed;
-        let date = data.coord.dt
-
-        //Creating and Appending the data to the Current Weather Card
-        let nameEl = $("<h3>").addClass("card-title").text(cityName + " " + moment(date).format('MMMM Do YYYY'));
-        const cardHeader = $("<div class='card-header'>")
-        cardHeader.append(nameEl)
-        $(".currentWeather").append(cardHeader)
-        let currentTemp = $("<p>").addClass("card-text").text("Temp: " + temp + '\xB0' + 'F')
-        $(".currentWeather").append(currentTemp);
-        let currentHumidity = $("<p>").addClass("card-text").text("Humidity: " + humidity + "%")
-        $(".currentWeather").append(currentHumidity);
-        let currentWind = $("<p>").addClass("card-text").text("Wind: " + wind + "MPH")
-        $(".currentWeather").append(currentWind);
-
-        //Grabbing the coords for the five day function
-        let coords = {
-            lat: data.coord.lat,
-            lon: data.coord.lon
-        }
-
-        getFiveDay(coords);
-    })
-
-
-}
-
-function getFiveDay(coords){
-    //API for Five Day 
-    const getFiveDayUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&exclude=current,minutely,hourly,alerts&units=imperial&appid=${apiKey}`
-
-    fetch(getFiveDayUrl)
-    .then((response)=> response.json())
-    .then((data)=>{
-        console.log("data from FIVE DAY", data.daily[0])
+    var cities = []
+    
+    $("#searchBtn").on("click", function(){
+        let citySearch = $("#searchInput").val()
+        if (citySearch === ""){
+            return null
+        }  
         
-        //UV Index for the Current Weather (In here so I donmt have to pull another API to just get the UV)
-        let uvIndex = $("<p>").addClass("card-text").text("UV Index: " + data.daily[0].uvi)
-        $(".currentWeather").append(uvIndex)
-        if(data.daily[0].uvi < 2) {
-            $(uvIndex).addClass("badge badge-pill low");
-        } else if (data.daily[0].uvi > 2 && data.daily[0].uvi <= 5) {
-            $(uvIndex).addClass("badge badge-pill moderate");
-        } else if (data.daily[0].uvi > 5 && data.daily[0].uvi <= 7) {
-            $(uvIndex).addClass("badge high");
-        } else if (data.daily[0].uvi > 7 && data.daily[0].uvi <= 10) {
-            $(uvIndex).addClass("badge vhigh");
-        } else {
-            $(uvIndex).addClass("xtreme");
-        };
+        //removing the hide classes 
+        $("#currentWeather").removeClass("hide")
+        $("#fiveDay").removeClass("hide")
+        $("#cTitle").removeClass("hide")
+        $("#fTitle").removeClass("hide")
+        
+        //Calling Current Weather
+        getCurrentWeather(citySearch)
+        
+        //Adding the searched Cities into the array
+        cities.push(citySearch)
+        savedCities();
 
-        //Running a loop to grab all the data
-        for(let i = 1; i < 6; i++){
-            let fiveDay = data.daily[i];
-            console.log(fiveDay)
-            let temp = data.daily[i].temp.day;
-            let humidity = data.daily[i].humidity;
-            let wind = data.daily[i].wind_speed;
-            let date = data.daily[i].dt;
-
-            //Creating cards and appending to them the five day data
-            const card = $("<div>").addClass("card col-md-2");
-            const cardHeader = $("<div>").addClass("card-header");
-            const cardBody = $("<div>").addClass("card-body");
-            const cardTitle = $("<div>").addClass("card-title").text(moment(date).format('MMMM Do YYYY'));
-            cardHeader.append(cardTitle);
-            cardBody.append(cardHeader);
-            card.append(cardBody);
-            $(".fiveDay").append(card);
-            let futureTemp = $("<p>").text("Temp: " + temp + '\xB0' + 'F');
-            cardTitle.append(futureTemp);
-            let futureHumidity = $("<p>").text("Humidity: " + humidity + "%");
-            cardTitle.append(futureHumidity);
-            let futureWind = $("<p>").text("Wind: " + wind + "MPH");
-            cardTitle.append(futureWind);
+        //Making a list of previous searched Cities
+        var storedCities = JSON.parse(localStorage.getItem("cities"))
+        for (var i = 0; i < cities.length; i++){
+            var city = storedCities[i]
+            var listCities = $("<li>").attr("id", "listCity").addClass("list-group-item").text(city);
+            $(".list-group").append(listCities);
         }
+            
+        
+        
+        //Clear value after search so you can't spam the same city
+        $("#searchInput").val("");
+   
+        //Clearing Out so it doesn't stack
+        $(".currentWeather").empty();
+        $(".fiveDay").empty();
+
     })
-}
+
+    function savedCities() {
+        //Saving to local storage
+        localStorage.setItem("cities", JSON.stringify(cities))
+    }
 
 
+    function getCurrentWeather(citySearch){
+        //API for Current Weather
+        const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&units=imperial&appid=${apiKey}`
+
+        fetch(currentWeatherUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("currentDay", data)
+            //Setting the data needed
+            let cityName = data.name;
+            let temp = data.main.temp;
+            let humidity = data.main.humidity;
+            let wind = data.wind.speed;
+            let date = data.coord.dt
+            //Grabbing the Icon
+            let icon = data.weather[0].icon;
+            let iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
+
+            //Creating and Appending the data to the Current Weather Card
+            let nameEl = $("<h3>").addClass("card-title").text(cityName + " " + moment(date).format('MMMM Do YYYY'));
+            const cardHeader = $("<div class='card-header'>")
+            cardHeader.append(nameEl)
+            $(".currentWeather").append(cardHeader)
+            let currentIcon = $("<img>").attr('src', iconUrl);
+            $(".currentWeather").append(currentIcon)
+            let currentTemp = $("<p>").addClass("card-text").text("Temp: " + temp + '\xB0' + 'F')
+            $(".currentWeather").append(currentTemp);
+            let currentHumidity = $("<p>").addClass("card-text").text("Humidity: " + humidity + " %")
+            $(".currentWeather").append(currentHumidity);
+            let currentWind = $("<p>").addClass("card-text").text("Wind: " + wind + " MPH")
+            $(".currentWeather").append(currentWind);
+
+        
+
+            //Grabbing the coords for the five day function
+            let coords = {
+                lat: data.coord.lat,
+                lon: data.coord.lon
+            }
+
+            getFiveDay(coords);
+        })
 
 
+    }
 
+    function getFiveDay(coords){
+        //API for Five Day 
+        const getFiveDayUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&exclude=current,minutely,hourly,alerts&units=imperial&appid=${apiKey}`
 
+        fetch(getFiveDayUrl)
+        .then((response)=> response.json())
+        .then((data)=>{
+            console.log("data from FIVE DAY", data.daily[0])
+        
+            //UV Index for the Current Weather (In here so I dont have to pull another API to just get the UV)
+            let uvIndex = $("<p>").addClass("card-text").text("UV Index: " + data.daily[0].uvi)
+            $(".currentWeather").append(uvIndex)
+            if(data.daily[0].uvi < 2) {
+                $(uvIndex).addClass("badge badge-pill low");
+            } else if (data.daily[0].uvi > 2 && data.daily[0].uvi <= 5) {
+                $(uvIndex).addClass("badge badge-pill moderate");
+            } else if (data.daily[0].uvi > 5 && data.daily[0].uvi <= 7) {
+                $(uvIndex).addClass("badge high");
+            } else if (data.daily[0].uvi > 7 && data.daily[0].uvi <= 10) {
+                $(uvIndex).addClass("badge vhigh");
+            } else {
+                $(uvIndex).addClass("xtreme");
+            };
 
+            //Running a loop to grab all the data
+            for(var i = 1; i < 6; i++){
+                let fiveDay = data.daily[i];
+                console.log(fiveDay)
+                let temp = data.daily[i].temp.day;
+                let humidity = data.daily[i].humidity;
+                let wind = data.daily[i].wind_speed;
+                let date = data.daily[i].dt;
+                let icon = data.daily[i].weather[0].icon
+                let iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
 
-
-
+                //Creating cards and appending to them the five day data
+                const card = $("<div>").addClass("card card-bg col-md-2");
+                const cardBody = $("<div>").addClass("card-body");
+                const cardTitle = $("<div>").addClass("card-title");
+                const cardText = $("<div>").addClass("card-text");
+                cardBody.append(cardTitle);
+                card.append(cardBody);
+                $(".fiveDay").append(card);
+                let futureDate = $("<h4>").text(moment(date).format('MMMM Do YYYY'));
+                cardTitle.append(futureDate);
+                let futureIcon = $("<img>").attr('src', iconUrl);
+                cardTitle.append(futureIcon)
+                let futureTemp = $("<p>").text("Temp: " + temp + '\xB0' + 'F');
+                cardText.append(futureTemp);
+                let futureHumidity = $("<p>").text("Humidity: " + humidity + " %");
+                cardText.append(futureHumidity);
+                let futureWind = $("<p>").text("Wind: " + wind + " MPH");
+                cardText.append(futureWind);
+                cardTitle.append(cardText)
+            }
+        })
+    }
+    $(document).on('click', '#listCity', function(){
+        var previousCity = $(this).attr("listCity")
+        getCurrentWeather(previousCity);
+        getFiveDay(previousCity);
+    })
 })
